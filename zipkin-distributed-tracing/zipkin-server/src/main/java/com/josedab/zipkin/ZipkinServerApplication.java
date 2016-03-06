ecb902043a4e33d75db70778cda13eaf9e3e53e8
Jose David Baena <josedab@gmail.com>
2016-03-06 13:28:06 -0800

    Zipkin: code formatting. No functional changeZipkin: code formatting. No functional changes

--- zipkin-distributed-tracing/zipkin-server/src/main/java/com/josedab/zipkin/ZipkinServerApplication.java
+++ zipkin-distributed-tracing/zipkin-server/src/main/java/com/josedab/zipkin/ZipkinServerApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 @SpringBootApplication
 public class ZipkinServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZipkinServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class, args);
+    }
 }
