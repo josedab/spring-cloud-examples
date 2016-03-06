70eb411b05ebb1d8b74c8b709612845e2b65fa1c
Jose David Baena <josedab@gmail.com>
2016-03-05 16:00:18 -0800

    Zipkin: base commit with current architecture

---
+++ zipkin-distributed-tracing/eureka-service/src/test/java/com/josedab/socialmedia/EurekaServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package com.josedab.socialmedia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.josedab.example.EurekaServiceApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = EurekaServiceApplication.class)
+public class EurekaServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
