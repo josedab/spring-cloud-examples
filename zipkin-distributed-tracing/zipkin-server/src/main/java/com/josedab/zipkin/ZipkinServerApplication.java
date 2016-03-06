f4772e33f79de5f0364f9500d6fa04dc927e07d1
Jose David Baena <josedab@gmail.com>
2016-03-05 19:38:23 -0800

    Zipkin: Added zipkin stream server - Stream server for the zipkin traces. No possibility to store. Just used as an example. If need to store, use open zipkin docker image

---
+++ zipkin-distributed-tracing/zipkin-server/src/main/java/com/josedab/zipkin/ZipkinServerApplication.java
@@ -0,0 +1,14 @@
+package com.josedab.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
+@EnableZipkinStreamServer
+@SpringBootApplication
+public class ZipkinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinServerApplication.class, args);
+	}
+}
