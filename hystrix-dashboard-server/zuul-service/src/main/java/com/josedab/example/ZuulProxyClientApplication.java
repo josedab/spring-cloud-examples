89524f4337f53f7cdfcd11f9a8250841eb1d61aa
Jose David Baena <josedab@gmail.com>
2017-02-04 12:05:50 -0800

    IS-16 Updated dependencies for dashboard server

--- hystrix-dashboard-server/zuul-service/src/main/java/com/josedab/example/ZuulProxyClientApplication.java
+++ hystrix-dashboard-server/zuul-service/src/main/java/com/josedab/example/ZuulProxyClientApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableCircuitBreaker
 @EnableZuulProxy
@@ -15,4 +17,5 @@ public class ZuulProxyClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZuulProxyClientApplication.class, args);
     }
+
 }
---
+++ hystrix-dashboard-server/zuul-service/src/main/java/com/josedab/example/config/ZuulConfiguration.java
@@ -0,0 +1,15 @@
+package com.josedab.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ZuulConfiguration {
+
+    @Bean
+    public RestTemplate defaultRestTemplate() {
+        return new RestTemplate();
+    }
+
+}
